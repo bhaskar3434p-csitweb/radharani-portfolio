0d3ebd34d5175fb7ffe82af24e666ef21941215d
bhaskar3434p-csitweb <bhaskar3434p@gmail.com>
2026-07-05 15:01:24 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+AOS.init({
+    duration:1000,
+    once:true
+});
+
+new Typed("#typing",{
+    strings:[
+        "Computer Science Student",
+        "Web Developer",
+        "AI & Research Enthusiast",
+        "Technology Explorer"
+    ],
+    typeSpeed:70,
+    backSpeed:45,
+    backDelay:1500,
+    loop:true
+});
